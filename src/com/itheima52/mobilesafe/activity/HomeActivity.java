542b7189b2d5fede530bbb74e27e2a4a81f2c9fd
anewboyz <36437110@qq.com>
2016-09-17 21:24:41 +0800

    主界面

---
+++ src/com/itheima52/mobilesafe/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.itheima52.mobilesafe.activity;
+
+import com.itheima52.mobilesafe.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Ö÷½çÃæ
+ * @author Administrator
+ *
+ */
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
